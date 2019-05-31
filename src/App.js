03b9f447b304a6f1ab985fe9496c72d2dbd9e6b9
slavotyk <gamelinkstv@gmail.com>
2019-05-31 21:23:59 +0300

    router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Landing from './components/layout/landing';
+
 function App() {
   return (
     <div className="App">
-
-        <h1>Hello GameDude</h1>
+        <BrowserRouter>
+            <Switch>
+                <Route exact path='/' component={Landing} />
+            </Switch>
+        </BrowserRouter>
 
     </div>
   );
---
+++ src/components/layout/landing.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Landing = () => {
+    return(
+        <h1> Hello GameDude </h1>
+    );
+};
+
+export default Landing;
