9158aa2b9a1093c70ae8453fff3513d0c9bd098b
slavotyk <gamelinkstv@gmail.com>
2019-05-31 22:15:27 +0300

    googleFontsAdd

---
+++ src/components/layout/landing/herotext.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Herotext = () => {
+    return(
+        <div className='heroText'>
+            Самый быстрый доступ к тому, что ты ищешь!
+        </div>
+    );
+};
+
+export default Herotext;
--- src/components/layout/landing/landing.js
+++ src/components/layout/landing/landing.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import Herotext from './herotext';
 
 const Landing = () => {
     return(
         <div className='mainContainer'>
             <div className='mainWrapper'>
                 {/*   Основной контент располагать ниже   */}
-
+                <Herotext/>
             </div>
         </div>
     );
