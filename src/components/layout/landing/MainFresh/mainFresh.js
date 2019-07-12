ce54d3d0bc88e9a5a5ece13ca511a811789934d1
lena-kovaleva <lena-mikhajlova@yandex.ru>
2019-07-12 19:56:26 +0200

    добавлена сортировка и отображение 8 последних игр

--- src/components/layout/landing/MainFresh/mainFresh.js
+++ src/components/layout/landing/MainFresh/mainFresh.js
@@ -13,34 +13,15 @@ const MainFresh = (props) => {
             <h2>Свежее</h2>
                 <div className="freshWrapper">
                     {
-                        (games || []).map(
-                            item => <Article key={ item.id } id = { item.id } title={ item.title } poster={ item.poster }/>
-                        )
+                        (games || [])
+                            .sort(
+                                (game1, game2) => game2.createdAt.seconds - game1.createdAt.seconds
+                            )
+                            .slice(0, 8)
+                            .map(
+                                item => <Article key={ item.id } id = { item.id } title={ item.title } poster={ item.poster }/>
+                            )
                     }
-                        {/* <div className="freshUnit freshUnit_1"
-                        data-title="Metro Exodus" data-description="продолжение легендарного шутера...">
-                    </div>
-                    <div className="freshUnit freshUnit_2"
-                        data-title="Dota 2" data-description="ну вы поняли...">
-                    </div>
-                    <div className="freshUnit freshUnit_3"
-                        data-title="Death stranding" data-description="Коджима-гений!...">
-                    </div>
-                    <div className="freshUnit freshUnit_4"
-                        data-title="MTG Arena" data-description="это вам не Harthstone...">
-                    </div>
-                    <div className="freshUnit freshUnit_4"
-                         data-title="MTG Arena" data-description="это вам не Harthstone...">
-                    </div>
-                    <div className="freshUnit freshUnit_3"
-                         data-title="Death stranding" data-description="Коджима-гений!...">
-                    </div>
-                    <div className="freshUnit freshUnit_1"
-                         data-title="Metro Exodus" data-description="продолжение легендарного шутера...">
-                    </div>
-                    <div className="freshUnit freshUnit_2"
-                         data-title="Dota 2" data-description="ну вы поняли...">
-                    </div> */}
                 </div>
         </div>
     );
