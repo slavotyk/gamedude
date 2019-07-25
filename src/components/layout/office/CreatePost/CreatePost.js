9cd380d6b9a558661d0d491797306abc345f129d
slavotyk <gamelinkstv@gmail.com>
2019-07-26 01:42:18 +0300

    postAddStylesLittleChanges

--- src/components/layout/office/CreatePost/CreatePost.js
+++ src/components/layout/office/CreatePost/CreatePost.js
@@ -10,23 +10,52 @@ class CreatePost extends Component {
     state = {
         title: '',
         game: '',
-        background: null,
         gameId: '',
-        content: ''
+        background: null,
+        coverPrev: null,
+        content: null
     };
 
     handleChange = (e) => {
+
+
+
         if (e.target.type === 'file') {
             this.setState({
                 [e.target.id]: e.target.files[0]
-            })
+            });
+
+            const file    = this.refs.uploadImg.files[0];
+            const reader  = new FileReader();
+
+            reader.onloadend = () => {
+                this.setState({
+                    coverPrev: reader.result
+                })
+            };
+            reader.readAsDataURL(file);
+            this.setState({
+                coverPrev :reader.result
+            });
+            console.log([e.target.coverPrev])
         }
         else {
             this.setState({
                 [e.target.id]: e.target.value
             })
         }
+
+
+
+
+
+
     };
+
+
+
+
+
 
     getSuggestions = value => {
         const { games } = this.props;
@@ -42,7 +71,7 @@ class CreatePost extends Component {
 
     gameChangeHandler = (gameId, game )=> {
         this.setState({gameId, game });
-    }
+    };
 
     handleSubmit = (e) => {
         e.preventDefault();
@@ -58,11 +87,15 @@ class CreatePost extends Component {
         return (
             <section>
                 <form onSubmit={this.handleSubmit} className="create-post">
-                        <input type="text" id="title" className="create-post__input" placeholder="Title" onChange={this.handleChange}/>
 
-                        <Autocomplete getSuggestions={this.getSuggestions} inputClassName={"create-post__input"} placeholder="Game" onChange={ this.gameChangeHandler } />
+                        <img className='create-post__background'  alt='Не забудьте загрузить фоновое изображение!' src={this.state.coverPrev}/>
+                        <div className='create-post__background_gradient'> </div>
 
-                        <input type="file" id="background"  className="create-post__input" placeholder="Background" onChange={this.handleChange}/>
+                        <input type="text" id="title" className="create-post__input create-post__input_title" placeholder="Заголовок поста" onChange={ this.handleChange }/>
+
+                        <Autocomplete getSuggestions={this.getSuggestions} inputClassName={"create-post__input"} placeholder="Выберите про какую игру ваш пост" onChange={ this.gameChangeHandler } />
+
+                        <input type="file" ref="uploadImg" id="background"  className="create-post__input" placeholder="Загрузите картинку" onChange={this.handleChange}/>
 
                         <textarea rows="20" id="content" className="create-post__textarea" placeholder="Content" onChange={this.handleChange}/>
 
