318ae14ed454d808d7e857ff1d57ba1b0aafbf69
vaceslavdacenko <v.dyachenko@offsidegaming.com>
2020-10-19 21:42:08 +0300

    GD-35 Подключить поддержку html внутри параграфов (ссылки / жирный / и тп)

--- src/components/layout/PostPage/PostDebouncer/PostDebouncer.js
+++ src/components/layout/PostPage/PostDebouncer/PostDebouncer.js
@@ -3,14 +3,17 @@ import React from 'react';
 
 const PostDebouncer = ({data, type}) => {
 
-    console.log(data);
+    // console.log(data);
+    function createMarkup() {
+        return {__html: data.text};
+    }
     if (type === 'header') {
         return (<h2 className='post-page__h2'>{data.text}</h2>)
     }
 
     return(
         <>
-            <p className='post-page__p'>{data.text}</p>
+            <p className='post-page__p' dangerouslySetInnerHTML={createMarkup()}/>
         </>
     )
 
