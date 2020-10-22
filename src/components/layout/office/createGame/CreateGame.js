83d5d3a4a805627337168e44aec3181641d8cc1c
vaceslavdacenko <v.dyachenko@offsidegaming.com>
2020-10-22 22:37:41 +0300

    bug fix small

--- src/components/layout/office/createGame/CreateGame.js
+++ src/components/layout/office/createGame/CreateGame.js
@@ -48,68 +48,68 @@ class CreateGame extends Component {
             <section>
                 <form onSubmit={this.handleSubmit} className="create-game-form">
                     <div>
-                        <label htmlFor="title">Game Title*</label>
+                        <label htmlFor="title" className='label'>Game Title*</label>
                         <input type="text" id='title' className="create-game-form__input" onChange={this.handleChange}/>
                     </div>
                     <div>
-                        <label htmlFor="developer">Game Developer*</label>
+                        <label htmlFor="developer" className='label'>Game Developer*</label>
                         <input type="text" id="developer" className="create-game-form__input" onChange={this.handleChange}/>
                     </div>
                     <div>
-                        <label htmlFor="category">Game Category*</label>
+                        <label htmlFor="category" className='label'>Game Category*</label>
                         <input type="text" id="category" className="create-game-form__input" onChange={this.handleChange}/>
                     </div>
                     <div>
-                        <label htmlFor="keywords">Keywords*</label>
+                        <label htmlFor="keywords" className='label'>Keywords*</label>
                         <input type="text" id="keywords" className="create-game-form__input" onChange={this.handleChange}/>
                     </div>
                     <div>
-                        <label htmlFor="poster">Game Poster*</label>
+                        <label htmlFor="poster" className='label'>Game Poster*</label>
                         <input type="file" id="poster" className="create-game-form__input" onChange={this.handleChange}/>
                     </div>
                     <div>
-                        <label htmlFor="background">Background*</label>
+                        <label htmlFor="background" className='label'>Background*</label>
                         <input type="file" id="background" className="create-game-form__input" onChange={this.handleChange}/>
                     </div>
                     <div>
-                        <label htmlFor="linkWeb">official WebPage*</label>
+                        <label htmlFor="linkWeb" className='label'>official WebPage*</label>
                         <input type="text" id="linkWeb" className="create-game-form__input" onChange={this.handleChange} />
                     </div>
                     <div>
-                        <label htmlFor="linkForum">official (or biggest) Forum</label>
+                        <label htmlFor="linkForum" className='label'>official (or biggest) Forum</label>
                         <input type="text" id="linkForum" className="create-game-form__input" onChange={this.handleChange}/>
                     </div>
                     <div>
-                        <label htmlFor="linkWiki">Wikipedia page*</label>
+                        <label htmlFor="linkWiki" className='label'>Wikipedia page*</label>
                         <input type="text" id="linkWiki" className="create-game-form__input" onChange={this.handleChange}/>
                     </div>
                     <div>
-                        <label htmlFor="linkVk">VK.com page</label>
+                        <label htmlFor="linkVk" className='label'>VK.com page</label>
                         <input type="text" id="linkVk" className="create-game-form__input" onChange={this.handleChange}/>
                     </div>
                     <div>
-                        <label htmlFor="linkYouTube">YouTube page</label>
+                        <label htmlFor="linkYouTube" className='label'>YouTube page</label>
                         <input type="text" id="linkYouTube" className="create-game-form__input" onChange={this.handleChange}/>
                     </div>
                     <div>
-                        <label htmlFor="linkTwitter">Twitter page</label>
+                        <label htmlFor="linkTwitter" className='label'>Twitter page</label>
                         <input type="text" id="linkTwitter" className="create-game-form__input" onChange={this.handleChange}/>
                     </div>
                     <div>
-                        <label htmlFor="linkFacebook">Facebook page*</label>
+                        <label htmlFor="linkFacebook" className='label'>Facebook page*</label>
                         <input type="text" id="linkFacebook" className="create-game-form__input" onChange={this.handleChange}/>
                     </div>
                     <div>
-                        <label htmlFor="linkInst">Instagram page</label>
+                        <label htmlFor="linkInst" className='label'>Instagram page</label>
                         <input type="text" id="linkInst" className="create-game-form__input" onChange={this.handleChange}/>
                     </div>
                     <div>
-                        <label htmlFor="linkTwitch">Twitch page</label>
+                        <label htmlFor="linkTwitch" className='label'>Twitch page</label>
                         <input type="text" id="linkTwitch" className="create-game-form__input" onChange={this.handleChange}/>
                     </div>
                     <div>
-                        <input type="submit" className="office__button" value="Добавить"/>
-                        <button className="office__button" onClick={ onCancel }>Отменить</button>
+                        <input type="submit" className="office__button office__button_newLine" value="Добавить"/>
+                        <button className="office__button office__button_newLine" onClick={ onCancel }>Отменить</button>
                     </div>
                 </form>
             </section>
